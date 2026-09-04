94672dccba487b59d91eef1daf88c1ed67baa5d9
mkcodedev <mohitsaraswat108@gmail.com>
2026-09-05 00:01:55 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://product-emi-switcher.vercel.app',
     credentials: true
   })
 );
